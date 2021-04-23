034c35f028109eb6db407214519e849d6658bdbe
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-23 19:29:42 -0300

    updated

--- interacao/vote.js
+++ interacao/vote.js
@@ -3,26 +3,22 @@ const db = require('quick.db')
 
 exports.run = async (client, message, args) => {
 
+    let prefix = db.get(`prefix_${message.guild.id}`)
+    if (prefix === null) prefix = "-"
+
     var content = args.join(' ')
-    if (!content) {
-        let prefix = db.get(`prefix_${message.guild.id}`)
-        if (prefix === null) prefix = "-"
 
-        var noargs = new Discord.MessageEmbed()
-            .setColor('#FF0000')
-            .setTitle('Siga o formato correto')
-            .setDescription('`' + prefix + 'votar O que você quer que seja votado.`')
+    if (content.length > 600) { return message.inlineReply(':x: O conteúdo a ser votado não pode passar de **600 caracteres.**') }
 
-        return message.inlineReply(noargs)
-    }
+    var embed = new Discord.MessageEmbed()
+        .setColor('BLUE')
+        .setTitle(`Votação aberta por ${message.author.username}`)
+        .setDescription(content)
+
+    if (!content) { return message.inlineReply('`' + prefix + 'votar O que você quer que seja votado.`') }
 
     if (content) {
-        var embed = new Discord.MessageEmbed()
-            .setColor('BLUE')
-            .setTitle(`Votação aberta por ${message.author.username}`)
-            .setDescription(content)
-
-        return message.inlineReply(embed).then(msg => {
+        return message.channel.send(embed).then(msg => {
             msg.react('👍').catch(err => { return })
             msg.react('👎').catch(err => { return })
         })
