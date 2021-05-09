774f7b1e633acf50837fee003e6cb63678118462
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-05-09 12:48:37 -0300

    Update dm.js

--- raphy/dm.js
+++ raphy/dm.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 
 exports.run = async (client, message, args) => {
 
-    let embed = new Discord.MessageEmbed()
+    const embed = new Discord.MessageEmbed()
         .setColor('#8B0000')
         .setTitle('Este comando está bloqueado sem previsão de volta')
         .setDescription('A equipe de proteção ao usuário do Discord pune os bots e os criadores por mensagens que não são rastreadas.\n \nUsuários costumam usar os bots com esses comandos para ofender e difamar pessoas anonimamente, sendo assim, o Discord desliga o bot e a conta do criador para sempre. \n \n> Por questão de segurança da Raphy, este comando está bloqueado.')
